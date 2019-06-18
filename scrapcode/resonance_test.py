d02cd0230e937225ea0fb218c2cc39dbe3f0cfd4
Sam <li887@wisc.edu>
2019-06-18 14:29:40 -0500

    Update resonance_test.py
    revised resonance_test

--- scrapcode/resonance_test.py
+++ scrapcode/resonance_test.py
@@ -12,8 +12,8 @@ from pulsestreamer import OutputState
 from pulsestreamer import PulseStreamer as Pulser 
 #import matplotlib.pyplot as plt
 import labrad
-import nidaqmx
-import nidaqmx.stream_writers as stream_writers
+#import nidaqmx
+#import nidaqmx.stream_writers as stream_writers
 
     
 actural_freqs = [2.85, 2.852, 2.854, 2.856, 2.858, 
@@ -25,38 +25,59 @@ num_steps = 21
 freq_center = 2.87  # GHz
 fm_dev = 0.034  # GHz
 uwave_power = 5.0  # dBm
-freq_range = 0.2
+freq_range = 0.4
+freq_deviation = freq_range/2
 
-freq_low = freq_center - freq_range
-freq_high = freq_center + freq_range
-#divide the frequencies range into subintervals so that each interval can be covered 
+freq_low = freq_center - freq_deviation
+freq_high = freq_center + freq_deviation
+
+#divide the frequencies range into intervals so that each interval can be covered 
 #by the fm deviation
-freq_list = numpy.linspace(freq_low, freq_high, int(freq_range//fm_dev)+2).tolist()   
+#in order to make sure that the whole frequency range has been included 
+freq_list = numpy.linspace(freq_low, freq_high, int(freq_deviation//fm_dev)+2).tolist()   
 
-def get_subgroups(Alist,length_step):
-    result = []
+#%%
+#this function returns a list of lists which each represents the interval that fm can cover
+#example: divide a frequency range over 2.67 to 3.07 and fm_range is 0.034*2
+#input:[2.67, 2.7366666666666664, 2.8033333333333332, 2.87, 2.9366666666666665, 
+#3.003333333333333, 3.07]
+#output: [[2.67, 2.7366666666666664], [2.8033333333333332, 2.87], [2.9366666666666665, 
+#3.003333333333333], [3.07]]
+def get_freq_subgroups(freq_list,fm_range):
+
+    #the master list of intervals we want to get 
+    freq_subgroups = []
+    
+    #loop over the freq_list except the last element
     ind = 0
-    while ind < len(Alist)-1:
-        sub_interval = [Alist[ind]]
-        if Alist[ind+1] - Alist[ind] > length_step:
-            result.append(sub_interval)
-            ind+=1
-        elif Alist[ind+1] - Alist[ind] <= length_step:                     
-            for ind2 in range(ind+1,len(Alist)):
-                if Alist[ind2]-Alist[ind] <= length_step:
-                    sub_interval.append(Alist[ind2])
+    while ind < len(freq_list)-1:
+        
+    #set the freq_interval to be a list containing the first frequency in an interval
+        freq_interval = [freq_list[ind]]
+        
+        #if the next frequency is within fm_deviation, go look at the rest frequencies
+        if freq_list[ind+1] - freq_list[ind] <= fm_range:                     
+        #add all the frequencies that are within the fm_deviation into the interval list    
+            for ind2 in range(ind+1,len(freq_list)):
+                if freq_list[ind2]-freq_list[ind] <= fm_range:
+                    freq_interval.append(freq_list[ind2])
                 else:                    
                     break                    
-            result.append(sub_interval)
-            ind += len(sub_interval)                
-        else:
-            ind+=1
-    if ind == len(Alist)-1:
-        result.append([Alist[len(Alist)-1]])     
-    return result
+        #once the fm_deviation is passed, an interval is found and append this interval to the master list    
+            freq_subgroups.append(freq_interval)
+            ind += len(freq_interval) #the loop will continue with next frequency after this interval            
+        
+        #end adding frequencies into the interval if the next freq is out of the fm_deviation 
+        elif freq_list[ind+1] - freq_list[ind] > fm_range:
+            freq_subgroups.append(freq_interval)
+            ind+=1 #go look at the next frequency if there is any        
+    
+    #if the last frequency has not been included, add it to the master list as an one-element list 
+    if ind == len(freq_list)-1:
+        freq_subgroups.append([freq_list[-1]])     
+    return freq_subgroups
 
-print(freq_list)
-
+#%%
 #ao_voltages = numpy.linspace(-1.0, +1.0, num_steps).tolist()
 
 #print(ao_voltages)
@@ -105,66 +126,38 @@ print(freq_list)
 #    cxn.microwave_signal_generator.uwave_off()
 #    state = OutputState([], 0.0, 0.0)
 #    pulser.constant(state)
+
 #%%
-def stopServer(self):
-    self.close_task_internal()
+#turn the microwave on and  set up the pulse streamer
+with labrad.connect() as cxn:
+    cxn.microwave_signal_generator.set_amp(uwave_power)
+    cxn.microwave_signal_generator.load_fm(fm_dev)
+    cxn.microwave_signal_generator.uwave_on()
+pulser = Pulser('128.104.160.11')
 
-def close_task_internal(self, task_handle=None, status=None,                            
-        callback_data=None):
-        task = self.task
-        if task is not None:
-            task.close()
-            self.task = None
-        return 0
-    
-def write(self,ao_voltages):
-    "Write the specified voltages to the output channels"
-    #Close previous task 
-    if self.task is not None:
-        self.close_task_internal()
-    with nidaqmx.Task() as task: 
-        #Set up the output channel 
-        task.ao_channels.add_ao_voltage_chan(self.ao_voltages,min_val = -1.0, max_val = +1.0)
-        task.write(ao_voltages)
+#Loop over each interval in the freq_subgroups
+#set +1,-1 corresponding to the max and min frequencies in each interval
+freq_sublist = get_freq_subgroups(freq_list,fm_dev*2)
+print(freq_sublist)
 
-
-def load_stream_writer(self,voltages):
-    
-    #Close the existing task 
-    if self.task is not None:
-        self.close_task_internal()
-    
-    #Write the initial voltages and stream the rest
-#    num_voltage = len(ao_voltages) #why do we need this? 
-    self.write(voltages)
-    stream_voltages = ao_voltages
-    
-    #Create a new task
-#    task = nidaqmx.Task(task_name)
-#    self.task = task
-#    
-    #Set up the output stream (set the output signal that is readable by the DAQ?)
-    out_stream = nidaqmx.task.OutStream(task) #this is a "Outsream" class object 
-    writer = stream_writrs.AnalogMultiChannelWriter(output_stream) #this is now a stream_writer class object
-    
-    writer.write_many_sample(stream_voltages)
-
-    
-#%%
-#iterate over a list of subintervals that cover the fm_dev 
-freq_sublist = get_subgroups(freq_list,fm_dev*2)
 for ind_sublist in range(len(freq_sublist)):
-#    if len(freq_sublist) %2 != 0: # for odd size sublist
-        freq_subcenter = (freq_sublist[ind_sublist][-1]-freq_sublist[ind_sublist][0])/2
-#        load_stream_writer(freq_sub,)
-        ao_voltages = numpy.linspace(-1.0, +1.0, len(freq_sublist[ind_sublist])).tolist()
-        load_stream_writer(ao_voltages)
-        if input('Enter nothing to continue or "q" to quit: ') == 'q':
-            break
     
+    #set the center frequency to be the mean value of frequencies in an interval
+    freq_subcenter = (freq_sublist[ind_sublist][-1]-freq_sublist[ind_sublist][0])/2
     
+    #match the voltages to the frequencies
+    ao_voltages = numpy.linspace(-1.0, +1.0, len(freq_sublist[ind_sublist])).tolist()
     
-    
+    #load the voltages into the microwave signal generater
+    cxn.microwave_signal_generator.set_freq(freq_subcenter)
+    state = OutputState([4], ao_voltages[ind_sublist], 0.0)
+    pulser.constant(state)
+    if input('Enter nothing to continue or "q" to quit: ') == 'q':
+        break    
+    cxn.microwave_signal_generator.mod_off()
+    cxn.microwave_signal_generator.uwave_off()
+    state = OutputState([], 0.0, 0.0)
+    pulser.constant(state)
     
     
     
